0d96b258225c30587a4dbee2dda35578986132b9
Brian Willis <brian@brianwillis.com>
2020-07-12 18:20:04 +1200

    Add a blank .net core console application.
    Using the standard template that comes with .net core. Hello world, indeed.

---
+++ Kryptos4.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kryptos4
+{
+    class Kryptos4
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
