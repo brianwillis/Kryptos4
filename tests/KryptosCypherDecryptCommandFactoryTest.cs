2f097924c43c4fd5db936529684301750c8d781d
Brian Willis <brian@brianwillis.com>
2020-07-13 22:28:52 +1200

    Add basic tests for KryptosCypherDecryptCommandFactory.
    Also creating a tests directory for future unit tests.

---
+++ tests/KryptosCypherDecryptCommandFactoryTest.cs
@@ -0,0 +1,29 @@
+using Xunit;
+
+namespace Kryptos4
+{
+    public class KryptosCypherDecryptCommandFactoryTest
+    {
+        [Fact]
+        public void FirstKeywordIsA()
+        {
+            var factory = new KryptosCypherDecryptCommandFactory();
+            var command = factory.GetNextCommand();
+            Assert.Equal("A", command.keyword);
+        }
+
+        [Fact]
+        public void KeywordIncrementsCorrectly()
+        {
+            var factory = new KryptosCypherDecryptCommandFactory();
+            var command = new KryptosCypherDecryptCommand();
+            
+            for (var i = 0; i <= 26; i++)
+            {
+                command = factory.GetNextCommand();
+            }
+
+            Assert.Equal("AA", command.keyword);
+        }
+    }
+}
