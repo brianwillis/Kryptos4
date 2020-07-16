9bbeb21697f8134d3e36c64f67ad799ea6434cff
Brian Willis <brian@brianwillis.com>
2020-07-16 20:23:45 +1200

    Add scoring.

--- Kryptos4SolutionScorer.cs
+++ Kryptos4SolutionScorer.cs
@@ -4,6 +4,117 @@ namespace Kryptos4
     {
         public void Score(KryptosCypherDecryptResult result) {
             result.score = 0;
+            var northeastPresentInCorrectPlace = IsNortheastPresentInCorrectPlace(result.solution);
+            var berlinPresentInCorrectPlace = IsBerlinPresentInCorrectPlace(result.solution);
+            var clockPresentInCorrectPlace = IsClockPresentInCorrectPlace(result.solution);
+
+            //100 points for having all three clues present and in the correct place.
+            if (northeastPresentInCorrectPlace && berlinPresentInCorrectPlace && clockPresentInCorrectPlace)
+            {
+                result.score = 100;
+                return;
+            }
+
+            //30 points each for having a clue present in the correct place.
+            //20 points each for having a clue present in the wrong place.
+            //10 points for having the characters that make up a clue scattered somewhere in the solution.
+            if (northeastPresentInCorrectPlace)
+            {
+                result.score += 30;
+            }
+            else if (IsNortheastPresentInWrongPlace(result.solution))
+            {
+                result.score += 20;
+            }
+            else if (IsNortheastScatteredInTheSolution(result.solution))
+            {
+                result.score += 10;
+            }
+
+            if (berlinPresentInCorrectPlace)
+            {
+                result.score += 30;
+            }
+            else if (IsBerlinPresentInWrongPlace(result.solution))
+            {
+                result.score += 20;
+            }
+            else if (IsBerlinScatteredInTheSolution(result.solution))
+            {
+                result.score += 10;
+            }
+
+            if (clockPresentInCorrectPlace)
+            {
+                result.score += 30;
+            }
+            else if (IsClockPresentInWrongPlace(result.solution))
+            {
+                result.score += 20;
+            }
+            else if (IsClockScatteredInTheSolution(result.solution))
+            {
+                result.score += 10;
+            }
+        }
+
+        private bool IsNortheastPresentInCorrectPlace(string solution)
+        {
+            return solution.Substring(25, 9) == "NORTHEAST";
+        }
+
+        private bool IsBerlinPresentInCorrectPlace(string solution)
+        {
+            return solution.Substring(63, 6) == "BERLIN";
+        }
+
+        private bool IsClockPresentInCorrectPlace(string solution)
+        {
+            return solution.Substring(69, 5) == "CLOCK";
+        }
+
+        private bool IsNortheastPresentInWrongPlace(string solution)
+        {
+            return !IsNortheastPresentInCorrectPlace(solution) && solution.Contains("NORTHEAST");
+        }
+
+        private bool IsBerlinPresentInWrongPlace(string solution)
+        {
+            return !IsBerlinPresentInCorrectPlace(solution) && solution.Contains("BERLIN");
+        }
+
+        private bool IsClockPresentInWrongPlace(string solution)
+        {
+            return !IsClockPresentInCorrectPlace(solution) && solution.Contains("CLOCK");
+        }
+
+        private bool IsNortheastScatteredInTheSolution(string solution)
+        {
+            return IsStringScatteredInSolution("NORTHEAST", solution);
+        }
+
+        private bool IsBerlinScatteredInTheSolution(string solution)
+        {
+            return IsStringScatteredInSolution("BERLIN", solution);
+        }
+
+        private bool IsClockScatteredInTheSolution(string solution)
+        {
+            return IsStringScatteredInSolution("CLOCK", solution);
+        }
+
+        private bool IsStringScatteredInSolution(string search, string solution)
+        {
+            var chars = search.ToCharArray();
+            foreach (var chr in chars)
+            {
+                if (!solution.Contains(chr))
+                {
+                    return false;
+                }
+                solution.Remove(solution.IndexOf(chr), 1);
+            }
+            return true;
         }
     }
 }
--- tests/Kryptos4SolutionScorerTest.cs
+++ tests/Kryptos4SolutionScorerTest.cs
@@ -17,7 +17,7 @@ namespace Kryptos4
         }
 
         [Fact]
-        public void Score_WhenGivenNortheast_ScoreEqualsThirty() {
+        public void Score_WhenGivenNortheastInRightPlace_ScoreEqualsThirty() {
             var result = new KryptosCypherDecryptResult {
                 solution = "                         NORTHEAST                                                               "
             };
@@ -29,7 +29,7 @@ namespace Kryptos4
         }
 
         [Fact]
-        public void Score_WhenGivenBerlin_ScoreEqualsThiry() {
+        public void Score_WhenGivenBerlinInRightPlace_ScoreEqualsThiry() {
             var result = new KryptosCypherDecryptResult {
                 solution = "                                                               BERLIN                            "
             };
@@ -41,7 +41,7 @@ namespace Kryptos4
         }
 
         [Fact]
-        public void Score_WhenGivenClock_ScoreEqualsThiry() {
+        public void Score_WhenGivenClockInRightPlace_ScoreEqualsThiry() {
             var result = new KryptosCypherDecryptResult {
                 solution = "                                                                     CLOCK                       "
             };
@@ -50,6 +50,78 @@ namespace Kryptos4
             scorer.Score(result);
 
             Assert.Equal(30, result.score);
+        }
+
+        [Fact]
+        public void Score_WhenGivenNortheastInWrongPlace_ScoreEqualsTwenty() {
+            var result = new KryptosCypherDecryptResult {
+                solution = "NORTHEAST                                                                                        "
+            };
+            var scorer = new Kryptos4SolutionScorer();
+
+            scorer.Score(result);
+
+            Assert.Equal(20, result.score);
+        }
+
+        [Fact]
+        public void Score_WhenGivenBerlinInWrongPlace_ScoreEqualsTwenty() {
+            var result = new KryptosCypherDecryptResult {
+                solution = "BERLIN                                                                                           "
+            };
+            var scorer = new Kryptos4SolutionScorer();
+
+            scorer.Score(result);
+
+            Assert.Equal(20, result.score);
+        }
+
+        [Fact]
+        public void Score_WhenGivenClockInWrongPlace_ScoreEqualsTwenty() {
+            var result = new KryptosCypherDecryptResult {
+                solution = "CLOCK                                                                                            "
+            };
+            var scorer = new Kryptos4SolutionScorer();
+
+            scorer.Score(result);
+
+            Assert.Equal(20, result.score);
+        }
+
+        [Fact]
+        public void Score_WhenGivenNortheastScattered_ScoreEqualsTen() {
+            var result = new KryptosCypherDecryptResult {
+                solution = " N O R T H E A S T                                                                               "
+            };
+            var scorer = new Kryptos4SolutionScorer();
+
+            scorer.Score(result);
+
+            Assert.Equal(10, result.score);
+        }
+
+        [Fact]
+        public void Score_WhenGivenBerlinScattered_ScoreEqualsTen() {
+            var result = new KryptosCypherDecryptResult {
+                solution = " B E R L I N                                                                                     "
+            };
+            var scorer = new Kryptos4SolutionScorer();
+
+            scorer.Score(result);
+
+            Assert.Equal(10, result.score);
+        }
+
+        [Fact]
+        public void Score_WhenGivenClockScattered_ScoreEqualsTen() {
+            var result = new KryptosCypherDecryptResult {
+                solution = " C L O C K                                                                                       "
+            };
+            var scorer = new Kryptos4SolutionScorer();
+
+            scorer.Score(result);
+
+            Assert.Equal(10, result.score);
         }
     }
 }
