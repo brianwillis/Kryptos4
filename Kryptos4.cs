f073a125b3bcd78b9ed74db9e9952f1a64861cd4
Brian Willis <brian@brianwillis.com>
2020-07-16 21:48:44 +1200

    Update main method to orchstrate all the functionality that's been built so far.
    I think we can call this a version one. Sill a lot to test.

--- Kryptos4.cs
+++ Kryptos4.cs
@@ -6,7 +6,30 @@ namespace Kryptos4
     {
         static void Main(string[] args)
         {
-            var factory = new KryptosCypherDecryptCommandFactory();
+            var factory = new DecryptCommandFactory();
+            var handler = new DecryptCommandHandler();
+            var scorer = new SolutionScorer();
+            var fileWriter = new SolutionFileWriter(); 
+            
+            var command = factory.GetNextCommand();
+            while (command != null) 
+            {
+                var result = handler.Solve(command);
+                scorer.Score(result);
+
+                if (result.score >= 30)
+                {
+                    fileWriter.Write(command, result);
+
+                    if (result.score == 100)
+                    {
+                        //We've found the correct solution, so we can stop.
+                        return;
+                    }
+                }
+
+                command = factory.GetNextCommand();
+            }
         }
     }
 }
